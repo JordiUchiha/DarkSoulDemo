06851918b45618f18cdeac69b9dea4b5dcbd6908
douch <douch@163.com>
2020-10-23 18:37:44 +0800

    玩家输入模块 模型导入 角色控制器 动画控制器提交

---
+++ Assets/Scripts/ActorController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorController : MonoBehaviour {
+
+    public GameObject model;
+    public float walkSpeed=1.4f;
+    private PlayerInput pi;
+    [SerializeField]
+    private Animator anim;
+    private Rigidbody rigid;
+    
+    private Vector3 movingVec;
+    
+	// Use this for initialization
+	void Start () {
+        anim = model.GetComponent<Animator>();
+        pi = GetComponent<PlayerInput>();
+        rigid = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        anim.SetFloat("forward", pi.Dmag* ((pi.run) ? 2.0f : 1.0f));
+        //角色转向
+        if(pi.Dmag>0.1f)
+        {
+            model.transform.forward = pi.Dright * transform.right + pi.Dup * transform.forward;
+        }
+        movingVec = pi.Dmag * model.transform.forward*walkSpeed*((pi.run)?2.0f:1.0f);
+	}
+
+    private void FixedUpdate()
+    {
+        //刚体相关在这里设置
+        rigid.position += movingVec * Time.fixedDeltaTime;
+        //rigid.velocity = new Vector3(movingVec.x, rigid.velocity.y, movingVec.z);
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+    [Header("===============Key Setting==========")]
+    public string KeyUp;
+    public string KeyDown;
+    public string KeyLeft;
+    public string KeyRight;
+
+    public string KeyA;
+    public string KeyB;
+    public string KeyC;
+    public string KeyD;
+
+    [Header("===============Output Signals==========")]
+    public float Dup;
+    public float Dright;
+    public float Dmag;
+    public bool run;
+    private float targetDup;
+    private float targetDright;
+
+    private float velocityDup;
+    private float velocityDright;
+
+    [Header("Others")]
+    public bool inputEnabled = true;
+
+    
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //key转为signal
+        //signal -1-1之间
+        targetDup = Input.GetKey(KeyUp)?1.0f:0 - (Input.GetKey(KeyDown)? 1.0f : 0);
+        targetDright = Input.GetKey(KeyRight) ? 1.0f : 0 - (Input.GetKey(KeyLeft) ? 1.0f : 0);
+
+        if(inputEnabled==false)
+        {
+            targetDright = 0.0f;
+            targetDup = 0.0f;
+        }
+
+        Dup = Mathf.SmoothDamp(Dup, targetDup, ref velocityDup, 0.1f);
+        Dright = Mathf.SmoothDamp(Dright, targetDright, ref velocityDright, 0.1f);
+
+        Dmag = Mathf.Sqrt(Dup * Dup + Dright * Dright);
+
+        run = Input.GetKey(KeyA);
+    }
+}
